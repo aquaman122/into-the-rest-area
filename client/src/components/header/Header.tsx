b4a9e0828d975acb6ee576dd5b044d5407ec191d
aquaman122 <swj981128@gmail.com>
2024-05-31 14:06:02 +0900

    feat: craco 설치

---
+++ client/src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default function Header() {
+  return (
+    <HeaderStyle>
+      <h2>Header</h2>
+    </HeaderStyle>
+  );
+}
+
+const HeaderStyle = styled.div``;
