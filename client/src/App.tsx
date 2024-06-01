2112f96f715bae19ccc31a2770dcdbff290ad04b
aquaman122 <swj981128@gmail.com>
2024-06-01 20:58:56 +0900

    feat: 글로벌 스타일링

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
-import Header from '@/components/header/Header';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/globalstyles';
+import { theme } from './styles/theme';
 
 function App() {
   return (
     <>
-      <Header />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <h1>app</h1>
+      </ThemeProvider>
     </>
   );
 }
