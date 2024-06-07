3fdd7b9f4780812c509011d61452b3e6c4b57df3
aquaman122 <swj981128@gmail.com>
2024-06-07 21:53:50 +0900

    feat: 헤더 컴포넌트 생성

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,9 +1,10 @@
+import Header from '@/components/Header';
 import styled from 'styled-components';
 
 const Main = () => {
   return (
     <MainStyle>
-      
+      <Header />
     </MainStyle>
   );
 };
