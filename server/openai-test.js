704fe2f79708fde3df6112c84deb3c9f649de965
aquaman122 <swj981128@gmail.com>
2024-06-13 12:12:46 +0900

    chore: openai 다운

---
+++ server/openai-test.js
@@ -0,0 +1,16 @@
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "user", content: "구구단 javascript로 만들어 줘" }],
+    model: "gpt-3.5-turbo",
+  });
+
+  console.log(completion.choices[0]);
+}
+
+main();
